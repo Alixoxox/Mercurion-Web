ec1f1e6805232aa26a4db906099783f0ca207db0
Alixoxox <Alixoxox@users.noreply.github.com>
2026-07-21 15:39:56 +0500

    feat: auto-scroll to top on route navigation

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { errorInterceptor } from './core/interceptors/error.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
@@ -9,7 +9,10 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top' }),
+    ),
     provideHttpClient(withInterceptors([errorInterceptor])),
     provideAnimations(),
     provideToastr({ positionClass: 'toast-top-right', timeOut: 3000 }),
