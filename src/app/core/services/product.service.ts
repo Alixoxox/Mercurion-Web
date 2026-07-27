0f162046f170b0c6c4b4ad56f4afb6ab4fe1215b
Alixoxox <Alixoxox@users.noreply.github.com>
2026-07-27 10:43:32 +0500

    Angular 16 added http caching -> shareReplay

--- src/app/core/services/product.service.ts
+++ src/app/core/services/product.service.ts
@@ -1,23 +1,35 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable, map, shareReplay } from 'rxjs';
 import { Product } from '../../shared/models/product';
 
 @Injectable({ providedIn: 'root' })
 export class ProductService {
   private http = inject(HttpClient);
-
+  private categories$? : Observable<string[]>;
+  private products$? : Observable<Product[]>
+  //caching -> shareReplay v16 - or - v19+ httpResource
   getAll(): Observable<Product[]> {
-    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+    if(!this.products$){
+    this.products$ = this.http.get<Product[]>('https://fakestoreapi.com/products').pipe(
+      shareReplay(1) //last emitted value
+    );
   }
+  return this.products$;
+}
 
   getById(id: number): Observable<Product | undefined> {
     return this.http.get<Product>(`https://fakestoreapi.com/products/${id}`);
   }
 
   getCategories(): Observable<string[]> {
-    return this.http.get<string[]>('https://fakestoreapi.com/products/categories');
+    if(!this.categories$){
+    this.categories$= this.http.get<string[]>('https://fakestoreapi.com/products/categories').pipe(
+      shareReplay(1)
+    );
   }
+  return this.categories$
+}
 
   getByCategory(category: string): Observable<Product[]> {
     return this.http.get<Product[]>(`https://fakestoreapi.com/products/category/${category}`);
